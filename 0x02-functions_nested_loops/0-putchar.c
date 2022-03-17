b4e69c25dc80114d49db7233f19b82c1797a6ed9
Odongoweri <99469707+Odongoweri@users.noreply.github.com>
2022-03-17 19:25:20 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,6 @@
 
 int main(void)
 {
-char name[] = "_putchar";
-int i;
-
-for (i = 0; i < 9; i++)
-{
-_putchar(name[i]);
-}
-_putchar('\n');
-return (0);
+        printf("_putchar\n");
+        return (0);
 }
