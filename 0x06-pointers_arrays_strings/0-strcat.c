ff634045145684210ff7220681799fc6874ab766
Odongoweri <99469707+Odongoweri@users.noreply.github.com>
2022-03-28 13:52:57 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,13 +9,12 @@
  */
 char *strcat(char *dest, const char *src)
 {
-	int index = 0, dest_len = 0;
+	int index, size;
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
+	for (size = 0; dest[size] != '\0'; size++)
+		;
+	for (index = size; src[index - size] != '\0'; index++)
+		dest[index] = src[index - size];
+	dest[index] = '\0';
 	return (dest);
 }
