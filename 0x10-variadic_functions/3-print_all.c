57e8819140d9da8726a5c4b7b78a0a1e3a0d6504
Odongoweri <99469707+Odongoweri@users.noreply.github.com>
2022-04-14 23:59:06 +0300

    m

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,85 +1,54 @@
-#include "variadic_functions.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
 
 /**
- * print_int - prints int
- * @list: arguments from print_all
- */
-void print_int(va_list list)
-{
-	printf("%d", va_arg(list, int));
-}
-
-/**
- * print_float - prints float
- * @list: arguments from print_all
- */
-void print_float(va_list list)
-{
-	printf("%f", va_arg(list, double));
-}
-
-/**
- * print_char - prints int
- * @list: arguments from print_all
- */
-void print_char(va_list list)
-{
-	printf("%c", va_arg(list, int));
-}
-
-/**
- * print_str - prints string
- * @list: arguments from print_all
- */
-void print_str(va_list list)
-{
-	char *s = va_arg(list, char *);
-
-	s == NULL ? printf("(nil)") : printf("%s", s);
-
-}
-
-/**
- * print_all - prints any type
- * @format: arguments to print
- */
+ * print_all - prints anything
+ * @format: any variable type
+ * Return: Nothing
+**/
 
 void print_all(const char * const format, ...)
 {
-va_list list;
-int i = 0, j = 0;
-char *sep = "";
-
-printTypeStruct printType[] = {
-	{ "i", print_int },
-	{ "f", print_float },
-	{ "c", print_char },
-	{ "s", print_str },
-	{NULL, NULL}
-};
-
-
-va_start(list, format);
+unsigned int i = 0, j = 0;
+char *str;
+va_list ls;
+va_start(ls, format);
 
 while (format && format[i])
 {
-	j = 0;
-	while (j < 4)
-	{
-		if (*printType[j].type == format[i])
-		{
-			printf("%s", sep);
-			printType[j].printer(list);
-			sep = ", ";
-			break;
-		}
-		j++;
-	}
-	i++;
+switch (format[i])
+{
+case 'c':
+printf("%c", va_arg(ls, int));
+break;
+case 'i':
+printf("%i", va_arg(ls, int));
+break;
+case 'f':
+printf("%f", va_arg(ls, double));
+break;
+case 's':
+str = va_arg(ls, char *);
+if (str == NULL)
+str = "(nil)";
+
+printf("%s", str);
+break;
+}
+j = i + 1;
+
+while (format[j] && (format[j] == 'c' || format[j] == 'i' ||
+format[j] == 'f' || format[j] == 's'))
+{
+printf(", ");
+break;
 }
 
+i++;
+}
 printf("\n");
-va_end(list);
+va_end(ls);
+return;
 }
